bf555e0070a34eb38a0aec6e94ee51363cf33643
daulet09 <146174463+daulet09@users.noreply.github.com>
2024-11-27 12:40:51 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_8yXoSGX0I', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5t5pf1jepqlb2e7c5g10meeno4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
